acbca026f240d512bcb003d86666df9fd28e2673
Mikolertesx <mgangsal@outlook.com>
2020-11-17 10:45:49 -0600

    Added fonts

--- App.js
+++ App.js
@@ -1,16 +1,35 @@
-import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
-
 import Header from "./components/Header";
-
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
 import GameOverScreen from "./screens/GameOverScreen";
 
+const fetchFonts = () => {
+  Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
+
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0);
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+        onError={(e) => console.log(e)}
+      />
+    );
+  }
 
   const startGameHandler = (selectedNumber) => {
     setUserNumber(selectedNumber);
